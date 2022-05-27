ec336af6e80958e70c363ae613ce05ed48650b9d
Your Name~ <murilopiresalves2@gmail.com>
2022-05-27 19:28:04 -0300

    first commit

---
+++ server/src/adapters/nodemailer/nodemailer-mail-adapter.ts
@@ -0,0 +1,22 @@
+import nodemailer from 'nodemailer';
+import { MailAdapter, SendMailData } from "../mail-adapter";
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "54493e4961b634",
+    pass: "884d67cbdd7b80"
+  }
+});
+
+export class NodemailerMailAdapter implements MailAdapter {
+  async sendMail({ subject, body }: SendMailData) {
+    await transport.sendMail({
+      from: 'Equipe Feedget <oi@feedget.com>',
+      to: 'Murilo Pires <murilopiresalves2@gmail.com>',
+      subject, 
+      html: body,
+    });
+  }
+}
